75d6e7fcf02e2ae31c0a4525bc1932a3d28997d1
Alexander <alexander.hyman05@gmail.com>
2023-07-13 02:45:13 -0700

    fix: uhh idek

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-require __DIR__ . "/inc/bootstrap.php";
+require dirname(__FILE__) . "/../" . "/inc/bootstrap.php";
 
 $uri = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
 $uri = explode("/", $uri);
