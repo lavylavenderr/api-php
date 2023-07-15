9fb8130b8b2ac2997df5a8b6d7f3f030b6cdaf3e
Alexander <alexander.hyman05@gmail.com>
2023-07-14 18:42:50 -0700

    fix: remove config.php from bootstrap.php

--- inc/bootstrap.php
+++ inc/bootstrap.php
@@ -4,8 +4,6 @@ define("PROJECT_ROOT_PATH", dirname(__FILE__) . "/../");
 
 require PROJECT_ROOT_PATH . "/vendor/autoload.php";
 
-require_once PROJECT_ROOT_PATH . "/inc/config.php";
-
 require_once PROJECT_ROOT_PATH . "/Controllers/BaseController.php";
 
 $dotenv = Dotenv\Dotenv::createImmutable(PROJECT_ROOT_PATH);
