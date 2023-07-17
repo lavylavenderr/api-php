ffd0afd1efa4cb55933f9ea5ae270f5e4dc3657d
Alexander <alexander.hyman05@gmail.com>
2023-07-16 23:34:08 -0700

    feat: spotify support, using redis

--- controllers/UtilController.php
+++ controllers/UtilController.php
@@ -1,8 +1,18 @@
 <?php
+use Predis\Client;
+
 class UtilController extends BaseController
 {
+    private $redis;
+    private $accessToken;
+    private $refreshToken;
+
     public function __construct()
     {
+        $this->redis = new Client("tcp://" . $_ENV["REDIS_URL"]);
+
+        $this->loadTokensFromRedis();
+
         if ($_SERVER["REQUEST_METHOD"] !== "GET") {
             $this->sendOutput(
                 json_encode([
@@ -12,6 +22,162 @@ class UtilController extends BaseController
                 ['Content-Type: application/json', 'HTTP/1.1 405 Method Not Allowed']
             );
         }
+    }
+
+    private function loadTokensFromRedis()
+    {
+        $this->accessToken = $this->redis->get("accessToken");
+        $this->refreshToken = $this->redis->get("refreshToken");
+    }
+
+    public function spotifyLoginAction()
+    {
+        $clientId = $_ENV["SPOTIFY_CLIENT_ID"];
+        $redirectUri = "http://localhost:8000/util/callback";
+
+        $scopes = "user-top-read";
+
+        $authUrl = "https://accounts.spotify.com/authorize" . "?" . http_build_query([
+            "client_id" => $clientId,
+            "response_type" => "code",
+            "redirect_uri" => $redirectUri,
+            "scope" => $scopes,
+        ]);
+
+        header("Location: " . $authUrl);
+        exit();
+    }
+
+    public function toptracksAction()
+    {
+        if (empty($this->accessToken)) {
+            $this->sendOutput(
+                json_encode(array("success" => false, "message" => "No Token")),
+                ["Content-Type: application/json", "HTTP/1.1 200 OK"]
+            );
+        }
+
+        $url = "https://api.spotify.com/v1/me/top/tracks?limit=10";
+
+        $headers = [
+            'Authorization: Bearer ' . $this->accessToken,
+            'Accept: application/json'
+        ];
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        $response = curl_exec($ch);
+        $this->sendOutput(
+            $response,
+            ["Content-Type: application/json", "HTTP/1.1 200 OK"]
+        );
+    }
+
+    public function callbackAction()
+    {
+        $params = $this->getQueryStringParams() ?: [];
+        $code = @$params["code"] ?: "";
+
+        if (empty($code)) {
+            $this->sendOutput(
+                json_encode(array("success" => false, "message" => "Invalid Code")),
+                ["Content-Type: application/json", "HTTP/1.1 400 Bad Request"]
+            );
+        }
+
+        $clientId = $_ENV["SPOTIFY_CLIENT_ID"];
+        $clientSecret = $_ENV["SPOTIFY_CLIENT_SECRET"];
+
+        $url = "https://accounts.spotify.com/api/token";
+        $credentials = $clientId . ':' . $clientSecret;
+        $encodedCredentials = base64_encode($credentials);
+
+        $data = [
+            "code" => $code,
+            "redirect_uri" => "http://localhost:8000/util/callback",
+            "grant_type" => "authorization_code"
+        ];
+
+        $fields = http_build_query($data);
+
+        $headers = [
+            'Authorization: Basic ' . $encodedCredentials,
+            'Content-Type: application/x-www-form-urlencoded',
+            'Accept: application/json'
+        ];
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        $response = curl_exec($ch);
+
+        $json = json_decode($response, true);
+
+        $this->accessToken = @$json["access_token"] ?: "";
+        $this->refreshToken = @$json["refresh_token"] ?: "";
+
+        if (empty($this->accessToken) || empty($this->refreshToken)) {
+            $this->sendOutput(
+                json_encode(array("success" => false, "message" => "Invalid response from token server")),
+                ["Content-Type: application/json", "HTTP/1.1 400 Bad Request"]
+            );
+        }
+
+        // Store the tokens in Redis
+        $this->redis->set("accessToken", $this->accessToken);
+        $this->redis->set("refreshToken", $this->refreshToken);
+
+        $this->sendOutput(
+            json_encode(array("success" => true, "message" => "OK")),
+            ["Content-Type: application/json", "HTTP/1.1 200 OK"]
+        );
+
+        $this->refreshToken($clientId, $clientSecret);
+    }
+
+    private function refreshToken($clientId, $clientSecret)
+    {
+        sleep(3600);
+
+        $url = "https://accounts.spotify.com/api/token";
+
+        $headers = [
+            'Authorization: Basic ' . $this->refreshToken,
+            'Content-Type: application/x-www-form-urlencoded',
+            'Accept: application/json'
+        ];
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        $response = curl_exec($ch);
+
+        $json = json_decode($response, true);
+
+        $this->accessToken = @$json["access_token"] ?: "";
+        $this->refreshToken = @$json["refresh_token"] ?: "";
+
+        // Store the tokens in Redis
+        $this->redis->set("accessToken", $this->accessToken);
+        $this->redis->set("refreshToken", $this->refreshToken);
+
+        return $this->refreshToken($clientId, $clientSecret);
     }
 
     public function mapAction()
@@ -50,7 +216,6 @@ class UtilController extends BaseController
             return;
         }
 
-
         curl_close($ch);
 
         $base64 = base64_encode($response);
@@ -60,6 +225,5 @@ class UtilController extends BaseController
             $imageData,
             ["Content-Type: image/png", "HTTP/1.1 200 OK"]
         );
-
     }
 }
--- inc/bootstrap.php
+++ inc/bootstrap.php
@@ -1,12 +1,20 @@
 <?php
 
+/* Root Dir so I don't gotta type that shit out  */
+
 define("PROJECT_ROOT_PATH", dirname(__FILE__) . "/../");
+
+/* Load Composer Dependencies */
 
 require PROJECT_ROOT_PATH . "/vendor/autoload.php";
 
+/* API Controller */
+
 require_once PROJECT_ROOT_PATH . "/controllers/BaseController.php";
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+/* .env Shit */
+
+$dotenv = Dotenv\Dotenv::createImmutable(PROJECT_ROOT_PATH);
 
 $dotenv->safeLoad();
 
