86c7fd48d8b5b8d6484053ce9f1a1c502825623b
Alexander Hyman <alexander.hyman05@gmail.com>
2023-07-21 01:05:27 -0700

    fix: short_term instead of medium

--- controllers/UtilController.php
+++ controllers/UtilController.php
@@ -64,11 +64,18 @@ class UtilController extends BaseController
             'Accept: application/json'
         ];
 
+        $data = [
+            "time_range" => "short_term",
+        ];
+
+        $fields = http_build_query($data);
+
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
         curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
         $response = curl_exec($ch);
