74aed05128cfe37bf740cf2ad87162b011e7eb8d
Lavender <alexander.hyman05@gmail.com>
2025-01-20 16:39:41 -0800

    Update spotify.php

--- routers/spotify.php
+++ routers/spotify.php
@@ -1,4 +1,5 @@
 <?php
+
 use Predis\Client;
 use Symfony\Component\HttpClient\HttpClient;
 
@@ -22,12 +23,7 @@ class SpotifyRouter extends BaseRouter
         $this->loadTokensFromRedis();
 
         if ($_SERVER["REQUEST_METHOD"] !== "GET" && $_SERVER["REQUEST_METHOD"] !== "POST") {
-            $this->respondWithJson(
-                json_encode([
-                    'success' => false,
-                    'message' => 'Method Not Allowed'
-                ])
-            );
+            $this->respondWithJson(json_encode(['success' => false, 'message' => 'Method Not Allowed']));
         }
     }
 
@@ -39,42 +35,28 @@ class SpotifyRouter extends BaseRouter
 
     private function refreshTokens()
     {
-        $clientId = $_ENV["SPOTIFY_CLIENT_ID"];
-        $clientSecret = $_ENV["SPOTIFY_CLIENT_SECRET"];
-
-        $credentials = $clientId . ':' . $clientSecret;
-        $encodedCredentials = \Delight\Base64\Base64::encode($credentials);
-
+        $credentials = \Delight\Base64\Base64::encode($_ENV["SPOTIFY_CLIENT_ID"] . ':' . $_ENV["SPOTIFY_CLIENT_SECRET"]);
         $url = "https://accounts.spotify.com/api/token";
         $response = $this->httpClient->request("POST", $url, [
             'query' => [
                 'refresh_token' => $this->redis->get("refreshToken"),
                 'grant_type' => 'refresh_token'
             ],
-            'headers' => [
-                'Authorization' => "Basic" . " " . $encodedCredentials
-            ],    
+            'headers' => ['Authorization' => "Basic " . $credentials],
         ]);
         $responseData = json_decode($response->getContent(), true);
-
-        $this->redis->set("accessToken", @$responseData["access_token"] ?: "");
+        $this->redis->set("accessToken", $responseData["access_token"] ?? "");
         $this->loadTokensFromRedis();
     }
 
     public function login()
     {
-        $clientId = $_ENV["SPOTIFY_CLIENT_ID"];
-        $redirectUri = $_ENV["REDIRECT_URL"];
-
-        $scopes = "user-top-read";
-
-        $authUrl = "https://accounts.spotify.com/authorize" . "?" . http_build_query([
-            "client_id" => $clientId,
+        $authUrl = "https://accounts.spotify.com/authorize?" . http_build_query([
+            "client_id" => $_ENV["SPOTIFY_CLIENT_ID"],
             "response_type" => "code",
-            "redirect_uri" => $redirectUri,
-            "scope" => $scopes,
+            "redirect_uri" => $_ENV["REDIRECT_URL"],
+            "scope" => "user-top-read",
         ]);
-
         header("Location: " . $authUrl);
         exit();
     }
@@ -83,47 +65,31 @@ class SpotifyRouter extends BaseRouter
     {
         if (!$this->redis->exists("accessToken")) {
             if (!$this->redis->exists("refreshToken")) {
-                $this->respondWithJson(json_encode(array("success" => false, "message" => "No Token")), 200);
+                $this->respondWithJson(json_encode(["success" => false, "message" => "No Token"]));
             } else {
                 $this->refreshTokens();
             }
         }
 
-        $params = $this->getQueryStringParams() ?: [];
-        $query = @$params["query"] ?: "";
-
-        if (empty($query))
+        $query = $this->getQueryStringParams()["query"] ?? "";
+        if (empty($query)) {
             return $this->respondWithJson(json_encode(['success' => false, 'message' => 'Missing Query']), 400);
+        }
 
-        $userQuery = urlencode($query);
-        $queryURL = "https://api.spotify.com/v1/search?q={$query}&type=track&limit=10";
-
+        $queryURL = "https://api.spotify.com/v1/search?q=" . urlencode($query) . "&type=track&limit=10";
         $response = $this->httpClient->request("GET", $queryURL, ['auth_bearer' => $this->accessToken]);
-        $statusCode = $response->getStatusCode();
 
-        if ($statusCode === 401) {
+        if ($response->getStatusCode() === 401) {
             $this->refreshTokens();
             $response = $this->httpClient->request("GET", $queryURL, ['auth_bearer' => $this->accessToken]);
         }
 
-        $responseData = json_decode($response->getContent(false), true);
-        $filteredTracks = [];
-
-        foreach ($responseData['tracks']['items'] as $item) {
-            $trackName = $item['name'];
-            $artistName = $item['artists'][0]['name'];
-            $albumCovers = $item['album']['images'];
-            $spotifyId = $item['id'];
-
-            $filteredTracks[] = [
-                'artist' => $artistName,
-                'track' => $trackName,
-                'album_covers' => $albumCovers,
-                'spotifyId' => $spotifyId
-            ];
-        }
-
-        /* https://api.spotify.com/v1/search?q=query&type=track&limit=10 */
+        $filteredTracks = array_map(fn($item) => [
+            'artist' => $item['artists'][0]['name'],
+            'track' => $item['name'],
+            'album_covers' => $item['album']['images'],
+            'spotifyId' => $item['id']
+        ], json_decode($response->getContent(), true)['tracks']['items']);
 
         $this->respondWithJson(json_encode(["success" => true, "data" => $filteredTracks]), 200);
     }
@@ -135,37 +101,26 @@ class SpotifyRouter extends BaseRouter
         }
 
         if (!$this->redis->exists("accessToken")) {
-            if (!this->redis->exists("refreshToken")) {
-                $this->respondWithJson(json_encode(array("success" => false, "message" => "No Token")), 200);
+            if (!$this->redis->exists("refreshToken")) {
+                $this->respondWithJson(json_encode(["success" => false, "message" => "No Token"]));
             } else {
-                $this->refreshToken();
+                $this->refreshTokens();
             }
         }
 
         $url = "https://api.spotify.com/v1/me/top/tracks?limit=10&time_range=short_term";
-        
         $response = $this->httpClient->request("GET", $url, ['auth_bearer' => $this->accessToken]);
-        $statusCode = $response->getStatusCode();
 
-        if ($statusCode === 401) {
+        if ($response->getStatusCode() === 401) {
             $this->refreshTokens();
             $response = $this->httpClient->request("GET", $url, ['auth_bearer' => $this->accessToken]);
         }
 
-        $responseData = json_decode($response->getContent(), true);
-        $filteredTracks = [];
-
-        foreach ($responseData['items'] as $item) {
-            $artistName = $item['artists'][0]['name'];
-            $trackName = $item['name'];
-            $albumCovers = $item['album']['images'];
-
-            $filteredTracks[] = [
-                'artist' => $artistName,
-                'track' => $trackName,
-                'album_covers' => $albumCovers
-            ];
-        }
+        $filteredTracks = array_map(fn($item) => [
+            'artist' => $item['artists'][0]['name'],
+            'track' => $item['name'],
+            'album_covers' => $item['album']['images']
+        ], json_decode($response->getContent(), true)['items']);
 
         $this->redis->set("topSongs", json_encode($filteredTracks), 'EX', 5 * 60 * 60);
         $this->respondWithJson(json_encode(["success" => true, "data" => $filteredTracks]), 200);
@@ -173,48 +128,36 @@ class SpotifyRouter extends BaseRouter
 
     public function callback()
     {
-        $params = $this->getQueryStringParams() ?: [];
-        $code = @$params["code"] ?: "";
+        $code = $this->getQueryStringParams()["code"] ?? "";
 
         if (empty($code)) {
-            $this->respondWithJson(json_encode(array("success" => false, "message" => "Invalid Code")), 200);
+            $this->respondWithJson(json_encode(["success" => false, "message" => "Invalid Code"]));
         }
 
-        $clientId = $_ENV["SPOTIFY_CLIENT_ID"];
-        $clientSecret = $_ENV["SPOTIFY_CLIENT_SECRET"];
-
-        $url = "https://accounts.spotify.com/api/token";
-        $credentials = $clientId . ':' . $clientSecret;
-        $encodedCredentials = \Delight\Base64\Base64::encode($credentials);
-
-        $response = $this->httpClient->request("POST", $url, [
-            'headers' => [
-                'Authorization' => "Basic" . " " . $encodedCredentials    
-            ],
+        $credentials = \Delight\Base64\Base64::encode($_ENV["SPOTIFY_CLIENT_ID"] . ':' . $_ENV["SPOTIFY_CLIENT_SECRET"]);
+        $response = $this->httpClient->request("POST", "https://accounts.spotify.com/api/token", [
+            'headers' => ['Authorization' => "Basic " . $credentials],
             'query' => [
                 'grant_type' => 'authorization_code',
                 'redirect_uri' => $_ENV["REDIRECT_URL"],
                 'code' => $code
             ]
         ]);
-        $statusCode = $response->getStatusCode();
 
-        if ($statusCode !== 200) {
-            return $this->respondWithJson(json_encode(["success" => false, "message" => "Invalid response from token server"]), 200);
+        if ($response->getStatusCode() !== 200) {
+            $this->respondWithJson(json_encode(["success" => false, "message" => "Invalid response from token server"]));
         }
 
         $responseData = json_decode($response->getContent(), true);
-        $this->accessToken = @$responseData["access_token"] ?: "";
-        $this->refreshToken = @$responseData["refresh_token"] ?: "";
+        $this->accessToken = $responseData["access_token"] ?? "";
+        $this->refreshToken = $responseData["refresh_token"] ?? "";
 
         if (empty($this->accessToken) || empty($this->refreshToken)) {
-            $this->respondWithJson(json_encode(array("success" => false, "message" => "Invalid response from token server")), 400);
+            $this->respondWithJson(json_encode(["success" => false, "message" => "Invalid response from token server"]));
         }
 
-        // Store the tokens in Redis
         $this->redis->set("accessToken", $this->accessToken);
         $this->redis->set("refreshToken", $this->refreshToken);
-
-        $this->respondWithJson(json_encode(array("success" => true, "message" => "OK")), 200);
+        $this->respondWithJson(json_encode(["success" => true, "message" => "OK"]), 200);
     }
 }
